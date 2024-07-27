7eba61a2a944f917a9ddcd05d5bfb5b25312768f
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-07-26 18:12:10 -0600

    𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const rl = createInterface(process.stdin, process.stdout)
 //const app = express()
 //const port = process.env.PORT || 8080;
 
-say('Manjiro\nDios\nSex', {
+say('Shadows\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Por Cristhofer`, {
+say(`Bot Created By ShadowVz`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
